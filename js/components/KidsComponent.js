b68df4cfdfbba9d3bb76fdc804ff6ae7ca2857e3
Mike Prior <mprior19xx@gmail.com>
2020-04-04 22:05:24 -0400

    style shells completed

--- js/components/KidsComponent.js
+++ js/components/KidsComponent.js
@@ -1,101 +1,150 @@
 export default {
     template: `
-    <component class="music-container">
-        <h1 class="hidden"> Kids Only Section </h1>
-        <div class="kids-gallery" id="kids-movies">
-            <h3 class="hidden">Kids Movie Gallery</h3>
+    <container class="container">
+        <h1 class="hidden">Kids Page Component</h1>
+        
+        <div class="gallery">
+            <h3 class="gallery-title">Most Popular Artists</h3>
 
-            <div class="kids-movie">
-                <h3 class="hidden">Kids Movie</h3>
-                <div class="kids-movie-description">
-                    <h4 class="hidden"> Kids Movie Description</h4>
+            <div class="gallery-main">
+                <h3 class="hidden">Main Gallery  - Promos & Favourites</h3>
+
+                <div class="large">
+                    <h3 class="hidden">Large Kids</h3>
                 </div>
+                <div class="large-desc">
+                        <h3 class="hidden">Kids Description</h3>
+                        <p>Kids descriptions go here</p>
+                    </div>
             </div>
 
-            <div class="kids-movie">
-                <h3 class="hidden">Kids Movie</h3>
-                <div class="kids-movie-description">
-                    <h4 class="hidden"> Kids Movie Description</h4>
-                </div>
-            </div>
+            <div class="gallery-sub">
+                <h3 class="gallery-sub-title">Even More Kids</h3>
 
-            <div class="kids-movie">
-                <h3 class="hidden">Kids Movie</h3>
-                <div class="kids-movie-description">
-                    <h4 class="hidden"> Kids Movie Description</h4>
-                </div>
-            </div>
+                <div class="genre" id="kids-genre-1">
+                    <h4 class="genre-title">Rock & Roll</h4>
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
 
-            <div class="kids-movie">
-                <h3 class="hidden">Kids Movie</h3>
-                <div class="kids-movie-description">
-                    <h4 class="hidden"> Kids Movie Description</h4>
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="genre" id="kids-genre-2">
+                    <h4 class="genre-title">Hip Hop</h4>
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="genre" id="kids-genre-3">
+                    <h4 class="genre-title">Jazz</h4>
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="genre" id="kids-genre-4">
+                    <h4 class="genre-title">Country</h4>
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
+
+                    <div class="mini">
+                        <h3 class="hidden">Small Kids</h3>
+                        <div class="small-desc">
+                            <h3 class="hidden">Kids Description</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-
-        <div class="kids-gallery" id="kids-tv">
-            <h3 class="hidden">Kids TV Show Gallery</h3>
-
-            <div class="kids-tv">
-                <h3 class="hidden">Kids TV Show</h3>
-                <div class="kids-tv-description">
-                    <h4 class="hidden"> Kids TV Show Description</h4>
-                </div>
-            </div>
-
-            <div class="kids-tv">
-                <h3 class="hidden">Kids TV Show</h3>
-                <div class="kids-tv-description">
-                    <h4 class="hidden"> Kids TV Show Description</h4>
-                </div>
-            </div>
-
-            <div class="kids-tv">
-                <h3 class="hidden">Kids TV Show</h3>
-                <div class="kids-tv-description">
-                    <h4 class="hidden"> Kids TV Show Description</h4>
-                </div>
-            </div>
-
-            <div class="kids-tv">
-                <h3 class="hidden">Kids TV Show</h3>
-                <div class="kids-tv-description">
-                    <h4 class="hidden"> Kids TV Show Description</h4>
-                </div>
-            </div>
-        </div>
-
-        <div class="kids-gallery" id="kids-music">
-            <h3 class="hidden">Kids Music Gallery</h3>
-
-            <div class="kids-music">
-                <h3 class="hidden">Kids Music</h3>
-                <div class="kids-music-description">
-                    <h4 class="hidden"> Kids Music Description</h4>
-                </div>
-            </div>
-
-            <div class="kids-music">
-                <h3 class="hidden">Kids Music</h3>
-                <div class="kids-music-description">
-                    <h4 class="hidden"> Kids Music Description</h4>
-                </div>
-            </div>
-
-            <div class="kids-music">
-                <h3 class="hidden">Kids Music</h3>
-                <div class="kids-music-description">
-                    <h4 class="hidden"> Kids Music Description</h4>
-                </div>
-            </div>
-
-            <div class="kids-music">
-                <h3 class="hidden">Kids Music</h3>
-                <div class="kids-music-description">
-                    <h4 class="hidden"> Kids Music Description</h4>
-                </div>
-            </div>
-        </div>
-    </component>
+    </container>
 `}
