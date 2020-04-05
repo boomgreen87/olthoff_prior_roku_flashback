4d0bf33b7f409aa37bafe4d3c6302645aeb44907
boomgreen87 <boomgreen87@yahoo.com>
2020-04-05 00:10:27 -0400

    Now able to create users

---
+++ admin/scripts/user.php
@@ -0,0 +1,28 @@
+<?php 
+function createUser($email, $fname, $lname, $country, $username, $password){
+    $pdo = Database::getInstance()->getConnection();
+    
+    // Creates a new user with provided data
+    $create_user_query = 'INSERT INTO tbl_accounts(accounts_email, accounts_first_name, accounts_last_name, accounts_country, accounts_username, accounts_password)';
+    $create_user_query .= ' VALUES(:email, :fname, :lname, :country, :username, :password)';
+
+    $create_user_set = $pdo->prepare($create_user_query);
+    $create_user_result = $create_user_set->execute(
+        array(
+            ':email'=>$email,
+            ':fname'=>$fname,
+            ':lname'=>$lname,
+            ':country'=>$country,
+            ':username'=>$username,
+            ':password'=>$password
+        )
+    );
+
+    //Redirects to index.php if user is created successfully. Otherwise, returns an error message.
+    if($create_user_result){
+        // redirect_to('index.php');
+        return 'Account created successfully!';
+    }else{
+        return 'The user did not go through';
+    }
+}
--- js/components/SignupComponent.js
+++ js/components/SignupComponent.js
@@ -10,41 +10,36 @@ export default {
                 
                     <div class="col-md-3 my-1">
                         <label class="sr-only" for="inlineFormInputName">Email</label>
-                        <input v-model="input.email" type="email" class="form-control" id="inlineFormInputName" placeholder="Email" required>
+                        <input v-model="input.email" type="email" class="form-control" id="signupEmail" placeholder="Email" required>
                     </div>
 
                     <div class="col-md-3 my-1">
                         <label class="sr-only" for="inlineFormInputName">First Name</label>
-                        <input v-model="input.firstname" type="text" class="form-control" id="inlineFormInputName" placeholder="First Name" required>
+                        <input v-model="input.firstname" type="text" class="form-control" id="signupFirstName" placeholder="First Name" required>
                     </div>
 
                     <div class="col-md-3 my-1">
                         <label class="sr-only" for="inlineFormInputName">Last Name</label>
-                        <input v-model="input.lastname" type="text" class="form-control" id="inlineFormInputName" placeholder="Last Name" required>
-                    </div>
-
-                    <div class="col-md-3 my-1">
-                        <label class="sr-only" for="inlineFormInputName">Birthday</label>
-                        <input v-model="input.birthday" type="text" class="form-control" id="inlineFormInputName" placeholder="Birthday" required>
+                        <input v-model="input.lastname" type="text" class="form-control" id="signupLastName" placeholder="Last Name" required>
                     </div>
 
                     <div class="col-md-3 my-1">
                         <label class="sr-only" for="inlineFormInputName">Country</label>
-                        <input v-model="input.country" type="text" class="form-control" id="inlineFormInputName" placeholder="Country" required>
+                        <input v-model="input.country" type="text" class="form-control" id="signupCountry" placeholder="Country" required>
                     </div>
 
                     <div class="col-md-3 my-1">
                         <label class="sr-only" for="inlineFormInputName">Username</label>
-                        <input v-model="input.username" type="text" class="form-control" id="inlineFormInputName" placeholder="Username" required>
+                        <input v-model="input.username" type="text" class="form-control" id="signupUsername" placeholder="Username" required>
                     </div>
 
                     <div class="col-md-3 my-1">
                         <label class="sr-only" for="inlineFormPassword">Password</label>
-                        <input v-model="input.password" type="password" class="form-control" id="inlineFormPassword" placeholder="Password" required>
+                        <input v-model="input.password" type="password" class="form-control" id="signupPassword" placeholder="Password" required>
                     </div>
 
                     <div class="col-auto my-1">
-                        <button type="submit" class="btn btn-primary">Go!</button>
+                        <button v-on:click.prevent="signup()" type="submit" class="btn btn-primary">Sign Up</button>
                     </div>
                 </div>
             </form>            
@@ -57,6 +52,10 @@ export default {
     data() {
         return {
             input: {
+                email: "",
+                firstname: "",
+                lastname: "",
+                country: "",
                 username: "",
                 password: ""
             },
@@ -65,33 +64,27 @@ export default {
     },
 
     methods: {
-        login() {
-            //console.log(this.$parent.mockAccount.username);
-            if(this.input.username != "" && this.input.password != "") {
-                let formData = new FormData();
+        signup() {
+            let formData = new FormData();
 
-                formData.append("username", this.input.username);
-                formData.append("password", this.input.password);
+            formData.append("email", this.input.email);
+            formData.append("firstname", this.input.firstname);
+            formData.append("lastname", this.input.lastname);
+            formData.append("country", this.input.country);
+            formData.append("username", this.input.username);
+            formData.append("password", this.input.password);
 
-                let url = "./includes/index.php?user=true";
+            let url = `./admin/admin_signup.php`;
 
-                fetch(url, {
-                    method: "POST",
-                    body: formData
-                })
-                .then(res => res.json())
-                .then(data => {
-                    console.log(data)
-                    // We get a user back, let's set authentication
-                    this.$emit("authenticated", true, data[0]);
-
-                    // Reroute to the users component so we can see all of them
-                    
-                })
-                .catch((error) => console.log(error))
-            } else {
-                console.log("A username and password should be entered")
-            }
+            fetch(url, {
+                method: "POST",
+                body: formData
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch((error) => console.log(error))
         }
     }
 }
