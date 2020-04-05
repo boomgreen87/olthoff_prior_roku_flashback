55aae903d45ab58c7a45338e3cb77ddcd9596796
boomgreen87 <boomgreen87@yahoo.com>
2020-04-04 22:03:38 -0400

    Added accounts and users tables to database. API folder may be defunct, kept in for now just in case we can use anything from it.

---
+++ admin/scripts/read.php
@@ -0,0 +1,14 @@
+<?php
+
+function getAll($tbl)
+{
+    $pdo = Database::getInstance()->getConnection();
+    $queryAll = 'SELECT * FROM ' . $tbl;
+    $results = $pdo->query($queryAll);
+
+    if ($results) {
+        return $results->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        return 'There was a problem accessing this info';
+    }
+}
